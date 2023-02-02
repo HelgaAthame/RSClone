c4fd248aecc9463ce8566a01853266619308be4a
HelgaAthame <athamethedarkest@gmail.com>
2023-02-02 16:34:40 +0300

    feat: add some styles

--- src/view/startView.ts
+++ src/view/startView.ts
@@ -1,5 +1,25 @@
+import './startView.scss';
+
 export class StartView {
   renderUI() {
-    // TO-DO
+    const main = document.createElement('main');
+    main.classList.add('main');
+    main.innerHTML = `
+      <section class="logo"></section>
+      <nav class="nav">
+        <div class="start">start</div>
+        <div class="continue">continue</div>
+      </nav>
+      <section class="top">
+        <div class="top__title">top</div>
+        <div class="top__score">00</div>
+      </section>
+      <footer class="footer">
+        <section class="github"></section>
+        <section class="year">2023</section>
+        <section class="rs"></section>
+      </footer>
+    `;
+    document.body.append(main);
   }
 }
